22ff7bd9ab1773bef44bda226a1d48c9d3e6cf44
harry-cmartin <harrysoncampos@gmail.com>
2025-04-10 00:43:31 -0300

    Teste no postman e restante do front

---
+++ serve_frontend.py
@@ -0,0 +1,25 @@
+import http.server
+import socketserver
+import os
+
+# Configuração do servidor
+PORT = 8080
+DIRECTORY = "frontend"
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+    
+    def end_headers(self):
+        # entender isso direito ESTUDAR
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
+        super().end_headers()
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"Servidor rodando em http://localhost:{PORT}")
+        httpd.serve_forever()
